7a6907206b13fc200f5ad68d60bf811206678268
CaptainCollie <shadaraevd@gmail.com>
2021-08-19 08:17:46 +0800

    add forth step

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,12 +1,13 @@
+"""Welcome user."""
 import prompt
 
 
 def welcome_user():
+    """Welcome user function."""
     print('Welcome to the Brain Games!')
-    name = prompt.string("May I have your name? ")
-    print(f"Hello, {name.capitalize()}")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name.capitalize()}')
 
 
 if __name__ == '__main__':
     welcome_user()
-
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
+"""Brain games executable file."""
 #!/usr/bin/env python
 from ..cli import welcome_user
 
+
 def main():
+    """Brain games welcome function."""
     welcome_user()
 
 
 if __name__ == '__main__':
     main()
-
