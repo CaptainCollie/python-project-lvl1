9359000273e84f1e6e63a225f6ab6c60be3eb85d
CaptainCollie <30548957+CaptainCollie@users.noreply.github.com>
2021-08-20 13:12:53 +0800

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -23,8 +23,8 @@ def progression_game():
         if answer == list_gen[rand_el]:
             print('Correct!')
         else:
-            correct = list_gen[rand_el]
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct}'.")
+            corr = list_gen[rand_el]
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{corr}'.")
             print(f"Let's try again, {name}!")
             flag = 0
             break
